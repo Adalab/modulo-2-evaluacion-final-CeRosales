830b07354203701399e4d2e0b53c1a219b512e59
Ce <celiarosalescaballero@gmail.com>
2022-04-08 10:39:29 +0200

    start project

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+/*La aplicación de búsqueda de cócteles consta de dos partes:
+. Un campo de texto y un botón para buscar un cóctel por su título.
+. Un listado de resultados de búsqueda donde aparece la imagen del cóctel y el nombre.*/
+
+
+
+
+fetch()
