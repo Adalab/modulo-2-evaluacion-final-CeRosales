ef157d78f8c2f046bcde0da12310368b1a507e52
Ce <celiarosalescaballero@gmail.com>
2022-04-08 21:15:11 +0200

    add paintHML

--- main.js
+++ main.js
@@ -7,5 +7,63 @@
 
 
 
+const searchButton = document.querySelector('.js-buttonSearch');
+const list = document.querySelector('.js-list');
+const input = document.querySelector('.js-input');
 
-fetch()
+
+/*function getCocktails(event) {
+  event.preventDefault();
+  fetch('https://www.thecocktaildb.com/', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((cardData) => {
+      console.log(cardData);
+    });
+
+
+    searchButton.addEventListener('click', getCocktails) ;*/
+
+    
+    let info = [];
+
+    function paintHTML() {
+    let html = '';
+       for (const drink of info) {
+        html += `<li>${drink.strDrink}</li>`   
+       }
+      list.innerHTML = html;
+    }
+
+   
+// filtrar elementos 
+// favoritos
+// localStorage
+// ev intermedia
+
+// B reset
+// maquetar
+    
+
+    function getCocktails() {
+     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then((response) => response.json())
+    .then((cocktailData) => { 
+        console.log(cocktailData);
+        info = cocktailData.drinks;
+        paintHTML();
+    });
+    };
+
+
+
+    
+    searchButton.addEventListener('click', getCocktails);
+
+    
+
