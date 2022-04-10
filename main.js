0739310cab2c0c57ea8a21f14ef481d4405d2247
Ce <celiarosalescaballero@gmail.com>
2022-04-10 20:55:55 +0200

    add favorites changes

--- main.js
+++ main.js
@@ -11,59 +11,144 @@ const searchButton = document.querySelector('.js-buttonSearch');
 const list = document.querySelector('.js-list');
 const input = document.querySelector('.js-input');
 
+      
 
-/*function getCocktails(event) {
-  event.preventDefault();
-  fetch('https://www.thecocktaildb.com/', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'content-type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((cardData) => {
-      console.log(cardData);
+let info = [];
+                                                    
+
+
+    //pinto 
+    
+function paintHTML(info) {
+    let html = '';
+
+    for (const drink of info) {
+    let classFavorite = '';
+        
+    const favoriteFoundIndex = favorites.findIndex(fav=>{
+        return fav.idDrink === drink.id;
     });
 
+    if(favoriteFoundIndex !== -1){
+        classFavorite = 'color-favorite'
+    }else {
+        classFavorite ='';
+    };
 
-    searchButton.addEventListener('click', getCocktails) ;*/
-
-    
-    let info = [];
-
-    function paintHTML() {
-    let html = '';
-       for (const drink of info) {
-        html += `<li>${drink.strDrink}</li>`   
-       }
-      list.innerHTML = html;
+    html += `<li class='js-drinks li ${classFavorite}' id=${drink.idDrink}>`;
+    html += `<img class='imagen' src=${drink.strDrinkThumb}>`;
+    html += `<h2 class ='h2'>${drink.strDrink}</h2>`;
+    html +=`</div>`;
+    html += `</li>`;
+            //falta cerrar div y li
     }
 
-   
-// filtrar elementos 
-// favoritos
-// localStorage
-// ev intermedia
+    list.innerHTML = html;
+    listenerCocktails();
+      
+      //listener de las bebidas cuando acabo de pintar
+};
 
-// B reset
-// maquetar
+   
+    
     
 
-    function getCocktails() {
-     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+
+// favoritos 
+
+//almacenaje en localstorage
+// reset
+// ev intermedia
+
+    
+
+//concatenar APIS
+// añadir imagen cuando no hay imagen
+
+
+
+
+//escuchar evento fav
+
+let favorites = [];
+
+
+
+function handleClickDrink(event){
+    console.log(event.currentTarget.id);
+    const idDrinks = event.currentTarget.id;
+   
+    const favoriteFound = info.find(fav =>{
+        return fav.idDrink === idDrinks;
+    });
+
+    const favoriteFoundIndex = favorites.findIndex(fav=>{
+        return fav.idDrink === idDrinks;
+    });
+
+    if (favoriteFoundIndex === -1) {
+        favorites.push(favoriteFound);
+        //agregar clase
+
+    }else { //eliminar drink de fav
+        favorites.splice(favoriteFoundIndex, 1);
+    } 
+    paintHTML();
+    console.log(favorites);
+};
+
+
+
+function listenerCocktails() {
+const liBebidas = document.querySelectorAll('.js-drinks');
+for (const item of liBebidas) {
+    item.addEventListener('click', handleClickDrink)
+        
+}};
+
+
+
+
+
+
+
+
+//filtrar
+
+function handleInput(event) {
+    event.preventDefault();
+    const filterValue = input.value ;
+    const listFilter = info.filter(palette => {
+        return palette.strDrink.toLowerCase().includes(filterValue.toLowerCase());
+    });
+   
+    paintHTML(listFilter);
+}
+
+input.addEventListener('keyup', handleInput);
+
+
+
+
+//está en localstorage?
+
+const listFilterStorage = JSON.parse(localStorage.getItem('listaBebidas'));
+
+if(listFilterStorage !== null) {
+    info = listFilterStorage;
+    paintHTML(info);
+
+}else {
+     let inputName = input.value;
+     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputName}')
     .then((response) => response.json())
     .then((cocktailData) => { 
         console.log(cocktailData);
         info = cocktailData.drinks;
-        paintHTML();
+        localStorage.setItem('listaBebidas', JSON.stringify(info))
+        paintHTML(info);
+       
     });
+   
     };
-
-
-
-    
-    searchButton.addEventListener('click', getCocktails);
-
-    
 
